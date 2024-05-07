ac3361f1f2d7e0a86fa0970174933e73a8e7982a
alinagunich2 <gunich.alina@gmail.com>
2024-05-07 14:48:23 +0300

    no message

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      borderWidth:{
+        '1px':'1px'
+      },
+      margin:{
+        '2px':'2px'
+      }
+    },
+  },
+  plugins: [],
+}
+
